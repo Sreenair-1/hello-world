38e1ab708b181e52cdf74720773704e1c3720238
Sreenair-1 <sreeharinair514@gmail.com>
2026-08-16 20:27:33 +0530

    Add test route to main.py and update Dockerfile to copy only main.py

--- main.py
+++ main.py
@@ -7,4 +7,9 @@ def run():
     print("User Authentication Feature Added")
     return "Hello World from Main Branch"
 
-app.run(host="0.0.0.0", port=5000)
+@app.route("/test")
+def test():
+    return "Application test successful"
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0", port=5000)
